d285f3c970c465008db16321fa2f5214d73843c1
PrimWILL <jiyoon0043@ajou.ac.kr>
2021-09-30 05:09:21 +0900

    FIX: don't show admin in user_list

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # SimpleSns
-Create SNS Service using django
+
+### ✍ 공부한 것
+* Django의 기본 구조 및 동작 방식
+* Django의 기본 제공 모델 (user 모델, tweet 모델) 사용  
+* Django의 ORM Model
+
+### 📑 제공 기능
+* 회원가입
+* 로그인/로그아웃
+* 팔로잉/팔로워
+* 글/댓글 작성
+* 태그
+
--- user/views.py
+++ user/views.py
@@ -65,7 +65,7 @@ def logout(request):
 def user_view(request):
     if request.method == 'GET':
         # 사용자를 불러오기, exclude와 request.user.username 를 사용해서 '로그인 한 사용자'를 제외하기
-        user_list = UserModel.objects.all().exclude(username=request.user.username)
+        user_list = UserModel.objects.all().exclude(username=request.user.username).exclude(username='admin')
         return render(request, 'user/user_list.html', {'user_list': user_list})
 
 
